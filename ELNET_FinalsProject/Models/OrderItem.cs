3110164a4b83aca52ef92dfd7bd37e0a1e103366
jhoncarl0 <jhoncarlocondor2@gmail.com>
2026-04-26 00:38:35 +0800

    Created core models including CartItem, Order, OrderItem, and User for cart, checkout, and authentication features

---
+++ ELNET_FinalsProject/Models/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿namespace ELNET_FinalsProject.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        public int MenuId { get; set; }
+        public Menu? Menu { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public int OrderId { get; set; }
+    }
+}
