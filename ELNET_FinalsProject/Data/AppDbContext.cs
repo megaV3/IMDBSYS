4961ecd4a0c7e80e2dfd5d1d0e34c2729258bc41
John <johnhvillarin@gmail.com>
2026-04-25 22:39:03 +0800

    added Menu model, connection string, DbContext, and EntityFrameworkCore

---
+++ ELNET_FinalsProject/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ELNET_FinalsProject.Models;
+
+namespace ELNET_FinalsProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Menu> Menus { get; set; }
+    }
+}
---
+++ ELNET_FinalsProject/Models/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ELNET_FinalsProject.Models
+{
+    public class Menu
+    {
+        public long? MenuID { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Category { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+        public string Description { get; set; } = String.Empty;
+        public bool CanBeHot { get; set; }
+        public bool CanBeCold { get; set; } 
+    }
+}
