a7c90a50d1294a07d205ea152c391960028ba841
DruvisIII <jannahgargar1@gmail.com>
2026-04-26 00:09:24 +0800

    Added MenuController

---
+++ ELNET_FinalsProject/Controllers/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELNET_FinalsProject.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
