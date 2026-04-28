abaf21568afadac49cfe8137c759d8151525dd4c
DruvisIII <jannahgargar1@gmail.com>
2026-04-29 00:28:35 +0800

    Deleted _LayoutHome file and Added Migrations to Update the Database

--- ELNET_FinalsProject/Migrations/20260427044710_InitialCreate.Designer.cs
+++ ELNET_FinalsProject/Migrations/20260427044710_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using ELNET_FinalsProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ELNET_FinalsProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260427044710_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
