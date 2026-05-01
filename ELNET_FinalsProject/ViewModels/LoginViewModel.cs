3f9f3e4accd1f1b41984bfbc7fb108dc37a123fe
John <johnhvillarin@gmail.com>
2026-05-01 22:36:39 +0800

    Changed Username to Email in LoginViewModel Changed Username to Email in asp-for

--- ELNET_FinalsProject/ViewModels/LoginViewModel.cs
+++ ELNET_FinalsProject/ViewModels/LoginViewModel.cs
@@ -4,9 +4,11 @@ namespace ELNET_FinalsProject.ViewModels
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "Username is required")]
-        public string Username { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
         [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
