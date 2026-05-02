4de8dcee8464b78916a9533d7b48cf24183a4857
John <johnhvillarin@gmail.com>
2026-05-02 21:06:17 +0800

    Created StoreViewModel for temporary use in StoreController

---
+++ ELNET_FinalsProject/ViewModels/StoreViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ELNET_FinalsProject.Models;
+
+namespace ELNET_FinalsProject.ViewModels
+{
+    public class StoreViewModel
+    {
+        public User UserProfile { get; set; } = new User();
+        public List<Menu> Menus { get; set; } = new List<Menu>();
+    }
+}
