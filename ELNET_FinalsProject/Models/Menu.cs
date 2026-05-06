f4efa78aa043fbe07bc52e0744fc206d84c7e4d1
DruvisIII <jannahgargar1@gmail.com>
2026-05-06 18:41:08 +0800

    Restored deleted files

---
+++ ELNET_FinalsProject/Models/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ELNET_FinalsProject.Models
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+        public bool CanBeHot { get; set; }
+        public bool CanBeCold { get; set; }
+
+        public string? ImagePath { get; set; }
+    }
+}
+
---
+++ ELNET_FinalsProject/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ELNET_FinalsProject.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        public int UserId { get; set; }
+
+        [Required]
+        public string CustomerName { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal TotalAmount { get; set; }
+
+        public string PaymentMethod { get; set; } = string.Empty;
+
+        public bool IsCompleted { get; set; }
+
+        public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
