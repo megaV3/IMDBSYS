db3024515d042cbc2109411ea7352fd3710805a4
John <johnhvillarin@gmail.com>
2026-05-08 14:45:11 +0800

    Added Balance property to ProfileViewModel
    Added TopUpViewModel

---
+++ ELNET_FinalsProject/ViewModels/TopUpViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ELNET_FinalsProject.ViewModels
+{
+    public class TopUpViewModel
+    {
+        [Required]
+        [Range(1, 10000)]
+        public decimal Amount { get; set; }
+        // You can add PaymentMethod, etc. here
+    }
+}
