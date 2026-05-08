b165cb1a4af0bb3dcf693acbc65de96627d79a7b
John <johnhvillarin@gmail.com>
2026-05-08 14:43:59 +0800

    Added TopUpHistory table

--- ELNET_FinalsProject/Data/AppDbContext.cs
+++ ELNET_FinalsProject/Data/AppDbContext.cs
@@ -14,5 +14,6 @@ namespace ELNET_FinalsProject.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<TopUpHistory> TopUpHistories { get; set; }
     }
 }
