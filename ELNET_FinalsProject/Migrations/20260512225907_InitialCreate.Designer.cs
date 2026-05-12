4440053e656f47dbcfd6f40b9802f00a6645f496
jhoncarl0 <jhoncarlocondor2@gmail.com>
2026-05-13 07:06:34 +0800

    Change color

--- ELNET_FinalsProject/Migrations/20260512225907_InitialCreate.Designer.cs
+++ ELNET_FinalsProject/Migrations/20260512225907_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IMDBSYS.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260512224019_seeddatachanges")]
-    partial class seeddatachanges
+    [Migration("20260512225907_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
