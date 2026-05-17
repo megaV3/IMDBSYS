508d248d05a047ac1319ccfce4b351b3003f2523
jhoncarl0 <jhoncarlocondor2@gmail.com>
2026-05-17 23:07:54 +0800

    Redesign the edit and new item

--- ELNET_FinalsProject/ViewModels/TopUpViewModel.cs
+++ ELNET_FinalsProject/ViewModels/TopUpViewModel.cs
@@ -5,7 +5,7 @@ namespace IMDBSYS.ViewModels
     public class TopUpViewModel
     {
         [Required(ErrorMessage = "Please enter an amount.")]
-        [Range(1, 10000, ErrorMessage = "You can only top up between ₱1 and ₱10,000.")]
+        [Range(1, 10000, ErrorMessage = "You can only top up between ₱1 - ₱10,000.")]
         public decimal? Amount { get; set; }
         // You can add PaymentMethod, etc. here
         public string Email { get; set; }
