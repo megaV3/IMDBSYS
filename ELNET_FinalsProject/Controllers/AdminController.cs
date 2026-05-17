791abc210694228abe30a49101148a7c7dc56abf
John <johnhvillarin@gmail.com>
2026-05-17 08:45:15 +0800

    Added AdminController for admins of the system

---
+++ ELNET_FinalsProject/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using IMDBSYS.Data;
+using IMDBSYS.Models;
+using IMDBSYS.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+
+namespace IMDBSYS.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
